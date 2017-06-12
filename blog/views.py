d892b82a1cc62fc996cf4ad11f51bd478b59a489
don mcroni <kojo_mcroni@hotmail.com>
2017-06-12 14:52:36 +0100

    changed the html for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('damn')
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
